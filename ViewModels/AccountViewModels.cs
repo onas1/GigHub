5a48168e441a505b27a0867ad041030ebe41dd1e
onas1 <80133923+onas1@users.noreply.github.com>
2021-07-03 01:06:50 +0100

    added name to application user

---
+++ ViewModels/AccountViewModels.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.ViewModels
+{
+
+    public class AccountViewModels
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
--- ViewModels/ManageViewModels.cs
+++ ViewModels/ManageViewModels.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNet.Identity;
+﻿using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-namespace GigHub.Models
+namespace GigHub.ViewModels
 {
     public class IndexViewModel
     {
