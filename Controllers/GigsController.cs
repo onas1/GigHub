74fb5330b559256208bf3816729815aca8efa559
onas1 <80133923+onas1@users.noreply.github.com>
2021-07-04 16:10:03 +0100

    Implement view gigs I'm attending.

--- Controllers/GigsController.cs
+++ Controllers/GigsController.cs
@@ -1,6 +1,7 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
 using Microsoft.AspNet.Identity;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -14,6 +15,29 @@ namespace GigHub.Controllers
         public GigsController()
         {
             _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Attending()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Attendances
+                .Where(a => a.AttendeeId == userId)
+                .Select(a => a.Gig)
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+                .ToList();
+
+
+
+            var viewModel = new GigsViewModel
+            {
+                UpcomingGigs = gigs,
+                ShowActions = User.Identity.IsAuthenticated,
+                Heading = "Gigs I'm Attending"
+
+            };
+
+            return View("Gigs", viewModel);
         }
         [Authorize]
         public ActionResult Create()
@@ -49,5 +73,7 @@ namespace GigHub.Controllers
 
             return RedirectToAction("Index", "Home");
         }
+
+
     }
 }
